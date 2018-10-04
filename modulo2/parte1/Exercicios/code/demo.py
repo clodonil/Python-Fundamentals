0e9195fa32e1171e1a8c26949ec23d6c98fc899f
Clodonil Honorio Trigo <clodonil@gmail.com>
2018-10-04 19:47:58 -0300

    Create demo.py

---
+++ modulo2/parte1/Exercicios/code/demo.py
@@ -0,0 +1,10 @@
+# importa a lib para conexão com o site Dados Abertos
+from  lib.scrapy_dadosAbertos import DadosAbertos
+# Inicia a class para obter os dados
+site_connect = DadosAbertos()
+# obtendo ajuda com os métodos disponíveis.
+print(site_connect.help())
+#Obtendo a lista de Deputados
+list_dep = site_connect.deputados()
+#Imprimindo a lista de deputados
+print(list_dep)
